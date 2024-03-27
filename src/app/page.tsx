6ef181e1ba3b16e39d9530c5891113127bb80322
Taylor Leach <taylorleach@hotmail.com>
2024-03-27 14:08:33 -0700

    added prettier and aligned eslint configs. formatted entire project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
 // import Image from "next/image";
 
-import Article from "./Article";
+import Article from './components/Article';
 
 export default function Home() {
   return (
-    <main className="max-w-2xl m-auto min-h-screen p-2">
+    <main className='max-w-2xl m-auto min-h-screen p-2'>
       <Article />
-      <div tabIndex={0} className="collapse bg-base-200 my-4">
-        <div className="collapse-title text-xl font-medium">
-          Focus me to see content
-        </div>
-        <div className="collapse-content">
+      <div tabIndex={0} className='collapse bg-base-200 my-4'>
+        <div className='collapse-title text-xl font-medium'>Focus me to see content</div>
+        <div className='collapse-content'>
           <p>tabIndex={0} attribute is necessary to make the div focusable</p>
         </div>
       </div>
-      <div className="gap-2 flex my-4">
-        <button className="btn btn-primary">Primary</button>
-        <button className="btn btn-secondary">Secondary</button>
+      <div className='gap-2 flex my-4'>
+        <button className='btn btn-primary'>Primary</button>
+        <button className='btn btn-secondary'>Secondary</button>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
 export default config;
