3c9c705d5d5d7a3925d0f666501d8d2bf697eeb2
Taylor Leach <taylorleach@hotmail.com>
2024-03-28 08:13:12 -0700

    attempt to static generate the posts at build time. added back favicon as next was using that as a param on the server requests causing confusing errors

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -7,3 +7,11 @@ export default function Page({ params }: { params: { slug: string } }) {
 export async function generateMetadata({ params: { slug } }: { params: { slug: string } }) {
   return await generateArticleMetadata(slug);
 }
+
+export async function generateStaticParams() {
+  // const posts = await fetch('https://.../posts').then((res) => res.json())
+  // return posts.map((post) => ({
+  //   slug: post.slug,
+  // }))
+  return [{ slug: 'javascript' }, { slug: 'css' }, { slug: 'html' }, { slug: 'react' }];
+}
